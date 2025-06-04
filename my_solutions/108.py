1b17909d2519dad799cd5caff4266af46d43a7da
riddik0331 <104014830+riddik0331@users.noreply.github.com>
2025-06-04 23:32:52 +0300

    Update 108.py

--- my_solutions/108.py
+++ my_solutions/108.py
@@ -5,18 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        if not nums:
-            return None
-        med = len(nums) // 2
-        node_val = nums[med]
-        node_left = nums[:med]
-        node_right = nums[med + 1:]
-        node = TreeNode(
-            val=node_val,
-            left=self.sortedArrayToBST(node_left),
-            right=self.sortedArrayToBST(node_right)
-            )
-        return node
+    # def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]: # recursive method
+    #     if not nums:
+    #         return None
+    #     mid = len(nums) // 2
+    #     node_val = nums[mid]
+    #     node_left = nums[:mid]
+    #     node_right = nums[mid + 1:]
+    #     node = TreeNode(
+    #         val=node_val,
+    #         left=self.sortedArrayToBST(node_left),
+    #         right=self.sortedArrayToBST(node_right)
+    #         )
+    #     return node
 
 
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:    #  Iterative method
+        stack = [(0, len(nums) - 1, None, False)]
+        while stack:
+            start, end, parent, is_left = stack.pop()
+            if start > end:
+                continue
+            mid = (start + end) // 2
+            node = TreeNode(nums[mid])
+            if parent:
+                if is_left:
+                    parent.left = node
+                else:
+                    parent.right = node
+            else:
+                root = node
+            stack.append((mid + 1, end, node, False))
+            stack.append((start, mid - 1, node, True))
+        
+        return root
