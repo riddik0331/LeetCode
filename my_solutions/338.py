d5472ab0e0f51c6ab8d36bf3b3ed485c7aa8c773
riddik0331 <104014830+riddik0331@users.noreply.github.com>
2025-06-24 18:33:03 +0300

    Create 338.py

---
+++ my_solutions/338.py
@@ -0,0 +1,13 @@
+class Solution:
+    # def countBits(self, n: int) -> List[int]:
+    #     res = []
+    #     for i in range(n + 1):
+    #         res.append(bin(i).count('1'))
+    #     return res
+
+
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(n + 1):
+            res[i] = res[i >> 1] + (i & 1)
+        return res
