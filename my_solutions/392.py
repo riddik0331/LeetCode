867d405477f4433368e27ff769d17e809f5d8d82
riddik0331 <104014830+riddik0331@users.noreply.github.com>
2025-06-29 00:08:19 +0300

    Update 392.py

--- my_solutions/392.py
+++ my_solutions/392.py
@@ -1,27 +1,41 @@
 class Solution:
+    # def isSubsequence(self, s: str, t: str) -> bool:
+    #     idx_s = 0
+    #     idx_t = 0
+    #     len_s = len(s)
+    #     len_t = len(t)
+    #     flag = False
+
+    #     if len_s == 0:
+    #         return True
+
+    #     while idx_s < len_s:
+    #         while idx_t < len_t:
+    #             if s[idx_s] == t[idx_t]:
+    #                 idx_t += 1
+    #                 flag = True
+    #                 break
+    #             flag = False
+    #             idx_t += 1
+    #         if idx_s == len_s -1 and flag:
+    #             return True
+    #         elif idx_s == len_s - 1 and not flag:
+    #             return False
+    #         elif idx_t == len_t:
+    #             return False
+    #         idx_s += 1
+
+
     def isSubsequence(self, s: str, t: str) -> bool:
-        idx_s = 0
-        idx_t = 0
-        len_s = len(s)
-        len_t = len(t)
-        flag = False
-
-        if len_s == 0:
+        if not s:
             return True
-
-        while idx_s < len_s:
-            while idx_t < len_t:
-                if s[idx_s] == t[idx_t]:
-                    idx_t += 1
-                    flag = True
-                    break
-                flag = False
-                idx_t += 1
-            if idx_s == len_s -1 and flag:
-                return True
-            elif idx_s == len_s - 1 and not flag:
-                return False
-            elif idx_t == len_t:
-                return False
-            idx_s += 1
-            
+        if len(s) > len(t):
+            return False
+        s_idx = 0
+        for t_idx in range(len(t)):
+            if s[s_idx] == t[t_idx]:
+                if s_idx == len(s) - 1:
+                    return True
+                s_idx += 1
+        
+        return False
