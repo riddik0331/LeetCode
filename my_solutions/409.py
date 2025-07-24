e141acbc4d866eec95711ba2b1f8d392347bf587
riddik0331 <104014830+riddik0331@users.noreply.github.com>
2025-07-24 19:51:32 +0300

    Create 409.py

---
+++ my_solutions/409.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        res = ''
+        counter = Counter(s)
+        flag_center = False
+        for key in counter:
+            while counter[key] > 1:
+                counter[key] -= 2
+                res = key + res + key
+            if counter[key] == 1 and not flag_center:
+                flag_center = True
+                center = len(res) // 2
+                res = res[:center] + key + res[center:]
+
+        return len(res)
