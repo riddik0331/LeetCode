9b47ea14b3dc07e810170599d85f8d9b18abfd92
riddik0331 <104014830+riddik0331@users.noreply.github.com>
2025-07-27 14:24:52 +0300

    Update 409.py

--- my_solutions/409.py
+++ my_solutions/409.py
@@ -1,15 +1,30 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        res = ''
-        counter = Counter(s)
-        flag_center = False
-        for key in counter:
-            while counter[key] > 1:
-                counter[key] -= 2
-                res = key + res + key
-            if counter[key] == 1 and not flag_center:
-                flag_center = True
-                center = len(res) // 2
-                res = res[:center] + key + res[center:]
+    # def longestPalindrome(self, s: str) -> int:
+    #     res = ''
+    #     counter = Counter(s)
+    #     flag_center = False
+    #     for key in counter:
+    #         while counter[key] > 1:
+    #             counter[key] -= 2
+    #             res = key + res + key
+    #         if counter[key] == 1 and not flag_center:
+    #             flag_center = True
+    #             center = len(res) // 2
+    #             res = res[:center] + key + res[center:]
 
-        return len(res)
+    #     return len(res)
+
+
+        def longestPalindrome(self, s: str) -> int:
+            res = 0
+            counter = Counter(s)
+            flag_center = False
+            for key in counter:
+                while counter[key] > 1:
+                    counter[key] -= 2
+                    res += 2
+                if counter[key] == 1 and not flag_center:
+                    flag_center = True
+                    res += 1
+
+            return res
